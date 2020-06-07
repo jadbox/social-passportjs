9580f0b153c147a01f865e07e9e03be9d138918d
Jonathan Dunlap <jdunlap@outlook.com>
2020-06-07 13:07:45 -0700

    add missing files

---
+++ src/discord.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const Strategy = require('passport-discord').Strategy;
+
+// 'email', 
+const scopes = ['identify', /* 'connections', (it is currently broken)  'guilds', 'guilds.join' */];
+const prompt = 'consent';
+
+function setup(app, path) {
+	const callbackURL = `${path}/dcallback`;
+	// console.log('callbackURL', callbackURL);
+
+	passport.use(
+		new Strategy(
+			{
+				clientID: process.env.DISCORD_CLIENT,
+				clientSecret: process.env.DISCORD_SECRET,
+				callbackURL: callbackURL,
+				scope: scopes,
+				prompt: prompt,
+			},
+			function (accessToken, refreshToken, profile, done) {
+				process.nextTick(function () {
+					return done(null, profile);
+				});
+			}
+		)
+	);
+	
+  app.get('/discord', passport.authenticate('discord', { scope: scopes, prompt: prompt }), function (req, res) {});
+  app.get(
+    '/dcallback',
+    passport.authenticate('discord', { failureRedirect: '/' }),
+    function (req, res) {
+			// req.session.cookie.uuid = req.user.id;
+			res.cookie('uuid', 'd'+req.user.id);
+			res.cookie('provider', 'discord');
+			res.cookie('username', req.user.username);
+      res.redirect('/info');
+    } // auth success
+  );
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+	});
+	
+}
+
+module.exports = { setup };
---
+++ src/telegram.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const passport = require('passport');
+
+const {TelegramStrategy} = require('passport-telegram-official');
+
+const throwError = () => {
+  throw new TypeError(
+    "Please provide your credentials through BOT_TOKEN and BOT_NAME envivroment variable. Also set PORT to 80, because widget won't work otherwise."
+  );
+};
+
+const botToken = process.env.TELEGRAM_TOKEN || throwError();
+const botName = process.env.TELEGRAM_NAME || throwError();
+
+passport.use(
+  new TelegramStrategy({ botToken: botToken, passReqToCallback: true }, (req, user, done) => {
+    console.log(user);
+
+    req.user = user;
+    done(null, user);
+  })
+);
+
+function telesetup(app, path) {
+  // Here we create page with auth widget
+  /*
+  app.get('/', (req, res) => {
+    res.send(`<html>
+<head></head>
+<body>
+  <div id="widget">
+      <script 
+         async 
+         src="https://telegram.org/js/telegram-widget.js?2"
+         data-telegram-login="${botName}"
+         data-size="medium"
+         data-auth-url="/login"
+         data-request-access="write"
+       ></script>
+  </div>
+</body>
+</html>`);
+  });
+  */
+
+  
+  // app.use(passport.initialize());
+
+  app.use('/tlogin', passport.authenticate('telegram'), (req, res) => {
+    res.cookie('uuid', 't'+req.user.id);
+    res.cookie('provider', 'telegram');
+    res.cookie('username', req.user.username);
+
+    console.log('telegram', req.user);
+    res.redirect('/info');
+    
+    // res.send(`You logged in! Hello ${req.user.first_name}!`);
+    // res.redirect('/');
+  });
+}
+
+module.exports = { telesetup };
